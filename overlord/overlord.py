5b0f88804b8f4f250dd50b32b80dc648e21cea02
Abhi Agarwal <me@abhiagarwal.com>
2015-06-16 14:36:42 -0400

    Adding JSON endpoint

--- overlord/overlord.py
+++ overlord/overlord.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
+import os
+import requests
 from os import path, environ
 import json
-from flask import Flask, Blueprint, abort, jsonify, request, session
+from flask import Flask, Blueprint, abort, jsonify, request, session, render_template
 import celeryconfig
 from celery import Celery
 
@@ -24,6 +26,81 @@ def make_celery(app):
                 return TaskBase.__call__(self, *args, **kwargs)
     celery.Task = ContextTask
     return celery
+
+# Static
+@app.route("/")
+def homePage():
+    return render_template('index.html')
+
+@app.route("/<userId>/json")
+def getJSON(userId):
+    headers = {
+        'content-type': 'application/vnd.api+json', 
+        'accept': 'application/*, text/*', 
+        'x-api-key': os.environ['TNYU_API_KEY']
+    }
+    person = requests.get('https://api.tnyu.org/v2/people/' + userId, headers=headers, verify=False)
+    personData = json.loads(person.text)
+    if len(personData['data']['attributes']['roles']) > 0:
+        return jsonify({
+            'static': [
+            {
+                'name': 'triggerBuild',
+                'parameters': ['repository', 'branch'],
+                'path': '/task/server/triggerBuild/<repository>/<branch>',
+                'result': '/result/backup/triggerBuild/<task_id>',
+            }
+            ],
+            'server': [
+            {
+                'name': 'rebuildWikiPassword',
+                'parameters': [],
+                'path': '/task/server/rebuildWikiPassword',
+                'result': '/result/server/rebuildWikiPassword/<task_id>',
+            },
+            {
+                'name': 'monitorServices',
+                'parameters': [],
+                'path': '/task/server/monitorServices',
+                'result': '/result/server/monitorServices/<task_id>',
+            },
+            {
+                'name': 'monitorTechatNYUOrgWebsite',
+                'parameters': [],
+                'path': '/task/server/monitorTechatNYUOrgWebsite',
+                'result': '/result/server/monitorTechatNYUOrgWebsite/<task_id>',
+            },
+            {
+                'name': 'monitorCheckinWebsite',
+                'parameters': [],
+                'path': '/task/server/monitorCheckinWebsite',
+                'result': '/result/server/monitorCheckinWebsite/<task_id>',
+            }
+            ],
+            'backup': [
+            {
+                'name': 'backupMySQLWithHost',
+                'parameters': [],
+                'path': '/task/backup/backupMySQLWithHost',
+                'result': '/result/backup/backupMySQLWithHost/<task_id>',
+            },
+            {
+                'name': 'backupMySQLWithoutHost',
+                'parameters': [],
+                'path': '/task/backup/backupMySQLWithoutHost',
+                'result': '/result/backup/backupMySQLWithoutHost/<task_id>',
+            },
+            {
+                'name': 'backupMongo',
+                'parameters': [],
+                'path': '/task/backup/backupMongo',
+                'result': '/result/backup/backupMongo/<task_id>',
+            }
+            ]
+        })
+    # jsonify will do for us all the work, returning the
+    # previous data structure in JSON
+    return jsonify({'status': '401'})
 
 # Static
 @app.route("/task/static/<task>/<repository>/<branch>")
