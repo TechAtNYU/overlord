e647a6a2991459149d52a97694c32b903e4c2d20
Abhi Agarwal <me@abhiagarwal.com>
2015-10-28 12:17:41 -0400

    Updating events

--- overlord/events.py
+++ overlord/events.py
@@ -5,7 +5,6 @@ from pytnyu import TNYUAPI
 requests.packages.urllib3.disable_warnings()
 
 api = TNYUAPI()
-facebook_event_id = ''
 api_event_id = ''
 access_token = os.environ['FACEBOOK_ACCESS_TOKEN']
 
@@ -19,7 +18,7 @@ def get_event_information(api_event_id):
   event = event['data']
   return event
 
-def post_API_rsvp_data(event_id, user_id_list, api_event_id):
+def post_API_rsvp_data(user_id_list, api_event_id):
   event = get_event_information(api_event_id)
   for i in user_id_list:
     event['links']['rsvps']['linkage'].append({'type': 'people', 'id': i})
@@ -50,7 +49,23 @@ def api_people_facebook_id_to_id():
       facebookid_to_id[facebook_id] = person_id
   return facebookid_to_id
 
-def match_facebook_RSVP_to_API(event_id, access_token, api_event_id):
+def normalize_rsvp_url(event_information):
+  if 'attributes' in event_information and 'rsvpUrl' in event_information['attributes']:
+    if event_information['attributes']['rsvpUrl'][-1] == '/':
+      event_id = event_information['attributes']['rsvpUrl'].split('/')[-2]
+    else:
+      event_id = event_information['attributes']['rsvpUrl'].split('/')[-1]
+    return event_id
+  else:
+    print 'Missing Facebook rsvpUrl'
+    return False
+
+def match_facebook_RSVP_to_API(access_token, api_event_id):
+  event_information = get_event_information(api_event_id)
+  event_id = normalize_rsvp_url(event_information)
+  if not event_id:
+    return
+  
   facebook_url = 'https://graph.facebook.com/v2.4/{0}/attending?limit=10000&access_token={1}'
   events = requests.get(facebook_url.format(event_id, access_token), verify=False)
   events = json.loads(events.text)
@@ -60,7 +75,6 @@ def match_facebook_RSVP_to_API(event_id, access_token, api_event_id):
   for i in event_attendees:
     if i['id'] in facebookid_to_id:
       rsvp_users_to_put_in_api.append(facebookid_to_id[i['id']])
-  post_API_rsvp_data(event_id, rsvp_users_to_put_in_api, api_event_id)
+  post_API_rsvp_data(rsvp_users_to_put_in_api, api_event_id)
 
-# get_event_information(api_event_id)
-match_facebook_RSVP_to_API(facebook_event_id, access_token, api_event_id)
+match_facebook_RSVP_to_API(access_token, api_event_id)
