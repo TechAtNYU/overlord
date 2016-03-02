e685a8eb17c3e2a645b22afaa2cb61a1c1d53b62
Abhi Agarwal <me@abhiagarwal.com>
2016-03-02 12:18:05 -0500

    Removing unused features and cleaning up code
    Fixing requirements
    
    Adhering to max line size property

--- overlord/backup.py
+++ overlord/backup.py
@@ -4,28 +4,9 @@ from subprocess import call
 from overlord import celery
 
 
-@celery.task
-def backup_bd_mysql():
-    shell = spur.SshShell(
-        hostname=os.environ['TNYU_BD_SERVER_IP'],
-        username=os.environ['TNYU_BD_SERVER_USER'],
-        password=os.environ['TNYU_BD_SERVER_PASSWORD'],
-        missing_host_key=spur.ssh.MissingHostKey.accept,
-    )
-    with shell:
-        passwordCombination = "-p" + os.environ['TNYU_BD_MYSQL_PASSWORD']
-        shell.run(["mysqldump", "zurmo", "-u", "zurmo", passwordCombination,
-                   ">", "/root/backups/sql.sql"], cwd="/root/backups", allow_error=True)
-        shell.run(["git", "add", "."], cwd="/root/backups", allow_error=True)
-        shell.run(["git", "commit", "-am", '"Adding Updates"'],
-                  cwd="/root/backups", allow_error=True)
-        result = shell.run(["git", "push", "-u", "origin",
-                            "master"], cwd="/root/backups", allow_error=True)
-    if result.return_code > 4:
-        return False
-    return True
-
-
+# This task runs the MongoDB backup on our API server.
+# Then it commits the result to the "api-db-backups"
+# repository on Github.
 @celery.task
 def backup_mongo():
     shell = spur.SshShell(
@@ -35,20 +16,26 @@ def backup_mongo():
         missing_host_key=spur.ssh.MissingHostKey.accept,
     )
     with shell:
-        shell.run(["rm", "-rf", "dump"], cwd="/backup", allow_error=True)
-        shell.run(["mkdir", "dump"], cwd="/backup", allow_error=True)
-        shell.run(["mongodump", "-h", "localhost", "-o", "dump", "-u", os.environ['TNYU_API_MONGO_USER'],
-                   '-p', os.environ['TNYU_API_MONGO_PW']], cwd="/backup", allow_error=True)
-        shell.run(["git", "add", "."], cwd="/backup", allow_error=True)
+        api_dir = "/backup"
+        shell.run(["rm", "-rf", "dump"], cwd=api_dir, allow_error=True)
+        shell.run(["mkdir", "dump"], cwd=api_dir, allow_error=True)
+        shell.run(["mongodump", "-h", "localhost", "-o", "dump", "-u",
+                  os.environ['TNYU_API_MONGO_USER'],
+                  '-p', os.environ['TNYU_API_MONGO_PW']],
+                  cwd=api_dir, allow_error=True)
+        shell.run(["git", "add", "."], cwd=api_dir, allow_error=True)
         shell.run(["git", "commit", "-am", '"Adding Updates"'],
-                  cwd="/backup", allow_error=True)
+                  cwd=api_dir, allow_error=True)
         result = shell.run(["git", "push", "-u", "origin",
-                            "master"], cwd="/backup", allow_error=True)
+                            "master"], cwd=api_dir, allow_error=True)
     if result.return_code > 4:
         return False
     return True
 
 
+# This task runs the Jira backup on our Jira server.
+# Then it commits the result to the "jira-db-backups"
+# repository on Github.
 @celery.task
 def backup_jira():
     shell = spur.SshShell(
@@ -58,17 +45,24 @@ def backup_jira():
         missing_host_key=spur.ssh.MissingHostKey.accept,
     )
     with shell:
+        atlassian_dir = "/var/atlassian/application-data/jira/export"
         shell.run(["git", "add", "."],
-                  cwd="/var/atlassian/application-data/jira/export", allow_error=True)
+                  cwd=atlassian_dir,
+                  allow_error=True)
         shell.run(["git", "commit", "-am", '"Adding Updates"'],
-                  cwd="/var/atlassian/application-data/jira/export", allow_error=True)
+                  cwd=atlassian_dir,
+                  allow_error=True)
         result = shell.run(["git", "push", "-u", "origin", "master"],
-                           cwd="/var/atlassian/application-data/jira/export", allow_error=True)
+                           cwd=atlassian_dir,
+                           allow_error=True)
     if result.return_code > 4:
         return False
     return True
 
 
+# This task runs the Discuss backup on our Discuss server.
+# Then it commits the result to the "discuss-db-backups"
+# repository on Github.
 @celery.task
 def backup_discuss():
     shell = spur.SshShell(
@@ -78,12 +72,16 @@ def backup_discuss():
         missing_host_key=spur.ssh.MissingHostKey.accept,
     )
     with shell:
+        discourse_dir = "/var/discourse/shared/standalone/backups/default"
         shell.run(["git", "add", "."],
-                  cwd="/var/discourse/shared/standalone/backups/default", allow_error=True)
+                  cwd=discourse_dir,
+                  allow_error=True)
         shell.run(["git", "commit", "-am", '"Adding Updates"'],
-                  cwd="/var/discourse/shared/standalone/backups/default", allow_error=True)
+                  cwd=discourse_dir,
+                  allow_error=True)
         result = shell.run(["git", "push", "-u", "origin", "master"],
-                           cwd="/var/discourse/shared/standalone/backups/default", allow_error=True)
+                           cwd=discourse_dir,
+                           allow_error=True)
     if result.return_code > 4:
         return False
     return True
--- overlord/static.py
+++ overlord/static.py
@@ -6,6 +6,11 @@ token = os.environ['TNYU_CIRCLECI_API_TOKEN']
 client = circleclient.CircleClient(token)
 
 
+# This task runs our CircleCI builds. If you look at our
+# "celeryconfig.py" file you would see that we have
+# CircleCI builds that are scheduled for every 15 minutes, etc.
+# This basically is responsible for building all of our projects
+# and uploading them onto Rackspace Cloud Files in those increments.
 @celery.task
 def trigger_build(projectName, branchName):
     status = client.build.trigger('techatnyu', projectName, branchName)
