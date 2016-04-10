b479c4f81b615b3a97aee819ca967ea6ca55285d
Nauman Ahmad <nauman-ahmad@outlook.com>
2016-04-10 16:00:52 -0400

    Debug flag false for production

--- overlord/overlord.py
+++ overlord/overlord.py
@@ -250,4 +250,4 @@ celery = make_celery(app)
 
 if __name__ == "__main__":
     port = int(environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
